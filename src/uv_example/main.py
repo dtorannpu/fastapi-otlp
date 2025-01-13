479e07c841f9e4a73b0cca298d4c48e578dcf648
dtorannpu <dtorannpu@gmail.com>
2025-01-13 11:01:28 +0900

    更新

--- src/uv_example/main.py
+++ src/uv_example/main.py
@@ -1,5 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from sample.models import Base, User
+
+
 def main():
-    pass
+    engine = create_engine('sqlite:///test.db', echo=True)
+    Base.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        u1 = User(name='test1')
+        u2 = User(name='test2')
+
+        session.add_all([u1, u2])
+
+        session.commit()
 
 
 if __name__ == "__main__":
