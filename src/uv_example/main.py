f05e2dc00d65e59e8753c319ecd8a0290b97fb6d
dtorannpu <dtorannpu@gmail.com>
2025-01-19 12:45:33 +0900

    空行修正

--- src/uv_example/main.py
+++ src/uv_example/main.py
@@ -1,9 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
-
 from sample.api.api import api_router
 
 app = FastAPI()
+
 app.include_router(api_router)
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
