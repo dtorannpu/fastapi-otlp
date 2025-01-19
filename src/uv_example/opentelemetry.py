9a5bb645c44c8bb6c38c51c45638aae901b8d7bb
dtorannpu <dtorannpu@gmail.com>
2025-01-19 14:13:38 +0900

    不要ファイル削除

--- src/uv_example/opentelemetry.py
+++
@@ -1,8 +0,0 @@
-from opentelemetry import trace
-
-from opentelemetry.sdk.trace import TracerProvider
-
-
-def initialize_tracer_provider():
-    tracer_provider = TracerProvider()
-    trace.set_tracer_provider(tracer_provider)
