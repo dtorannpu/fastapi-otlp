9bdf296c6ed767393c58a5a7774a98af5f2c010e
dtorannpu <dtorannpu@gmail.com>
2025-01-19 13:43:36 +0900

    データ取得

--- packages/sample/src/sample/api/endpoints/users.py
+++ packages/sample/src/sample/api/endpoints/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from sample.api.deps import SessionDep
 from sample.crud import user
@@ -9,16 +9,24 @@ router = APIRouter()
 
 @router.get("", response_model=UsersResponse)
 def user_list(session: SessionDep) -> UsersResponse:
-    return UsersResponse.model_validate(user.get_all(session=session))
+    return UsersResponse.model_validate(user.user_list(session=session))
 
 
 @router.post("", response_model=UserResponse)
 def create_user(request: CreateUser, session: SessionDep) -> UserResponse:
     return UserResponse.model_validate(
-        user.create(session=session, create_user=request)
+        user.user_create(session=session, create_user=request)
     )
 
 
-@router.delete("")
-def delete_user():
-    pass
+@router.get("/{user_id}", response_model=UserResponse)
+def get_user_by_id(user_id: int, session: SessionDep) -> UserResponse:
+    u = user.get_user_by_id(session=session, user_id=user_id)
+    if u is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return UserResponse.model_validate(u)
+
+
+@router.delete("{user_id}")
+def delete_user(user_id: int, session: SessionDep):
+    user.user_delete(session=session, user_id=user_id)
--- packages/sample/src/sample/crud/user.py
+++ packages/sample/src/sample/crud/user.py
@@ -6,14 +6,27 @@ from sample.models.user import User
 from sample.schemas.user import CreateUser
 
 
-def get_all(*, session: Session) -> Sequence[User]:
-    stmt = select(User).order_by(User.name)
-    return session.exec(stmt).all()
+def user_list(*, session: Session) -> Sequence[User]:
+    statement = select(User).order_by(User.name)
+    return session.exec(statement).all()
 
 
-def create(*, session: Session, create_user: CreateUser) -> User:
+def user_create(*, session: Session, create_user: CreateUser) -> User:
     db_obj = User.model_validate(create_user)
     session.add(db_obj)
     session.commit()
     session.refresh(db_obj)
     return db_obj
+
+
+def user_delete(*, session: Session, user_id: int):
+    statement = select(User).where(User.id == user_id)
+    user = session.exec(statement).one()
+    session.delete(user)
+    session.commit()
+
+
+def get_user_by_id(*, session: Session, user_id: int) -> User | None:
+    statement = select(User).where(User.id == user_id)
+    user = session.exec(statement).first()
+    return user
